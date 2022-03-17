2c74ee004d3c296b703229bfba2e175f6a5b5078
Tristan Constable <tristan.m.constable@gmail.com>
2022-03-17 14:59:02 +0200

    Setup of flask

--- Server.py
+++ Server.py
@@ -1,27 +1,7 @@
-import web
-from web import form
-from web.template import Render
-import Video_Fetch
+from flask import Flask
 
-urls = ('/', 'index')
+app = Flask(__name__)
 
-
-
-class index:
-    
-    def POST(self):
-        data = web.input()
-        print(data.URL)
-        path = Video_Fetch.DownloadVideo(data.URL)
-    
-    def GET(self):
-        render = web.template.render('templates/')
-        path = ""
-        visible = "true"
-        return render.FileInput(path, visible)
-
-    
-
-if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()  
+@app.route("/")
+def landing():
+    return "<h1>It works!</p>"
